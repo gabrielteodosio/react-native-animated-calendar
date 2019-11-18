d3d60939a0d106e697d22674e3a0ec03eaefe7ee
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2019-11-18 10:12:30 -0300

    exported components for usage

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 /**
  * @format
  */
-
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export { default as Calendar } from './src/Components/Agenda/Components/Calendar';
+export { default as Agenda } from './src/Components/Agenda';
