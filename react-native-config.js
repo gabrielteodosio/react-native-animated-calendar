a3d22ca8e26c69965472ce687a615dda4e93a068
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2019-12-02 01:39:47 -0300

    added react-native-config file

---
+++ react-native-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dependencies: {
+    'react-native-gesture-handler': {
+      platforms: {
+        ios: null,
+        android: null,
+      },
+    },
+  },
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+};
